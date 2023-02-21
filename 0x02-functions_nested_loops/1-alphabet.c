53bc0fce12937d4a24e0c7d663b8390502cea0d9
Nkqubela Nqalati <44855131+qubinqalati@users.noreply.github.com>
2023-02-21 23:30:36 +0200

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+      char la;
+  
+      for (la= 'a'; la <= 'z'; la++)
+            _putchar(la);
+  
+      _putchar('\n');
+}
