b48aef37f827e5de4e506871e94901ab49d6e04a
qubinqalati <qubinqalati@gmail.com>
2023-03-02 00:22:50 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * string_toupper - change lowercase to uppercase
  * @s:string
@@ -7,15 +6,15 @@
  */
 char *string_toupper(char *s)
 {
+
 	int i;
 
 i = 0;
-
 	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 			*(s + i) -= 'a' - 'A';
-		i++
+		i++;
 	}
 	return (s);
 }
