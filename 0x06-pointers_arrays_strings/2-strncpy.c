a48125574bb311db30f825554eeb476134ce989b
qubinqalati <qubinqalati@gmail.com>
2023-03-01 23:17:31 -0800

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ *
+ * @src: The source of the string
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0, i < n && *(src + 1); i++)
+	{
+		*(dest + 1) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
