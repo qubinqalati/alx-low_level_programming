36b36fc6634eab4314e1f2ce1324c824a4462ad5
qubinqalati <qubinqalati@gmail.com>
2023-07-13 17:58:35 +0000

    0x00

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - Entry
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
