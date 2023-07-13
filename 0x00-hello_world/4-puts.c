363b1b76e232e1a63ee96b50b8d97f990e32beb1
qubinqalati <qubinqalati@gmail.com>
2023-07-13 10:25:32 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main: Entry point
+ * Description: print a string using puts
+ * @char: the string to be printed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
