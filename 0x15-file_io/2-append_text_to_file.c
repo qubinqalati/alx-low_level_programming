ad68f7c929b2f4c546c236a37760a43374fcddb7
qubinqalati <qubinqalati@gmail.com>
2023-09-04 07:19:56 -0700

    0x15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,9 +32,9 @@ int append_text_to_file(const char *filename, char *text_cintent)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == 1)
+	if (fd == -1)
 		return (-1);
-	else if (text_content != NULL)
+	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (len == -1)
